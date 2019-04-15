95c3b4a364a357fb729c47b3fa2cce5a1a5fdc95
JeongHanYoung <8735132@gmail.com>
2019-04-15 20:58:04 +0900

    project init

---
+++ src/main/java/xyz/hanyoung/handlebarpractice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package xyz.hanyoung.handlebarpractice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
